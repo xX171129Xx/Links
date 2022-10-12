5f56c6736978be9e8c2cfa1421de5731f21652e9
Samson Orr <171129@student.vansd.org>
2022-10-12 18:23:00 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+//Code From https://developers.google.com/web/fundamentals/app-install-banners/native
+
+const installBtn = document.querySelector('#installBtn');
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can add to home screen
+  installBtn.style.display = 'inline-block';
+});
+
+installBtn.addEventListener('click', (e) => {
+  // hide our user interface that shows our A2HS button
+  installBtn.style.display = 'none';
+  // Show the prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice
+    .then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+});
